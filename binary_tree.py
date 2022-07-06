88e652195cb104f8d65c4e5524efd566005c7e95
Steven Garcia <sjosegarcia23@gmail.com>
2022-07-06 11:43:59 -0400

    insertion and search

--- binary_tree.py
+++ binary_tree.py
@@ -42,6 +42,34 @@ class BinaryTree:
         print(f'inorder: {self.inorder_traversal(self.root, "")}')
         print(f'postorder: {self.postorder_traversal(self.root, "")}')
 
+    
+    def search(self, node: Node, search: int) -> bool:
+        if node.value == search:
+            return True
+        if node.value > search and node.left:
+            return self.search(node.left, search)
+        if node.value < search and node.right:
+            return self.search(node.right, search)
+        return False
+
+    def insert(self, node: Node, value: int) -> None:
+        if not self.root:
+            self.root = node
+            return
+        if node.value > value:
+            if not node.left:
+                node.left = Node(value)
+                return
+            else:
+                self.insert(node.left, value)
+        elif node.value < value:
+            if not node.right:
+                node.right = Node(value)
+                return
+            else:
+                self.insert(node.right, value)
+        else:
+            print('Node exist')
 
 b_tree = BinaryTree(Node(1))
 b_tree.root.left = Node(2)
@@ -49,6 +77,18 @@ b_tree.root.right = Node(3)
 b_tree.root.left.left = Node(4)
 b_tree.root.left.right = Node(5)
 b_tree.root.right.left = Node(6)
-b_tree.root.left.right = Node(7)
+b_tree.root.right.right = Node(7)
 
+'''
+            1
+        /        \
+       2          3
+      / \        / \ 
+     4   5      6   7
+'''
+
+b_tree.print_orders()
+
+print(f'search: {b_tree.search(b_tree.root, 7)}')
+print(f'insert: {b_tree.insert(b_tree.root, 8)}')
 b_tree.print_orders()
